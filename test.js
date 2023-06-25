287da0db8c7e3b0d60f001217fbeae41d68f081a
balaOuO <99466599+balaOuO@users.noreply.github.com>
2023-06-26 02:20:32 +0800

    [add] random start line position

--- test.js
+++ test.js
@@ -1,5 +1,3 @@
-// document.write("<h1>請上傳照片</h1>")
-
 var button_isblue = true;
 var canvars;
 var ctx;
@@ -10,6 +8,7 @@ var now_cycle = 0, total_cycle;
 var speed = 0.001;
 var max_speed;
 var stop_flag = false;
+var start_btn = getElementById("start_but");
 
 function handleFiles(files) {
     const preview = document.getElementById("preview");
@@ -26,6 +25,7 @@ function handleFiles(files) {
         img_w = preview.width;
         img_h = preview.height;
         console.log(img_h, img_w);
+        line_pos = Math.random() * img_h;
     };
     reader.readAsDataURL(file);
 }
@@ -46,8 +46,6 @@ function speed_curve() {
 
 
 function draw() {
-    console.log(speed);
-    console.log(line_pos);
     now_cycle += 1;
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -85,7 +83,8 @@ function control(element) {
             now_cycle = 0;
             speed = 0.001
 
-            stop_flag = setInterval(draw, 25)
+            stop_flag = setInterval(draw, 15)
+
 
         }
         else if (!button_isblue) {
@@ -100,71 +99,4 @@ function control(element) {
         alert("圖都沒上傳是在start個雞巴");
     }
 
-    // preview.src = canvas.toDataURL();
 }
-
-// var button_isblue = true;
-// var canvas, context;
-// var lineColor = "red";
-// var lineWidth = 2;
-// var startX, startY, endX, endY;
-// var isDrawing = false;
-
-// function handleFiles(files) {
-//     const preview = document.getElementById("preview");
-//     const file = files[0];
-//     const reader = new FileReader();
-//     reader.onload = function (event) {
-//         preview.src = event.target.result;
-//     }
-//     reader.readAsDataURL(file);
-//     preview.onload = function () {
-//         preview.src = canvas.toDataURL();
-//     };
-// }
-
-// function control(element) {
-//     if (button_isblue) {
-//         button_isblue = false;
-//         element.style = "width:240px;height:80px;border:2px #860d09 dashed;";
-
-//         canvas = document.createElement("canvas");
-//         context = canvas.getContext("2d");
-//         canvas.width = preview.width;
-//         canvas.height = preview.height;
-//         context.drawImage(preview, 0, 0);
-
-//         startX = canvas.width / 2;
-//         startY = canvas.height / 2;
-//         endX = startX + 100;
-//         endY = startY + 100;
-//         isDrawing = true;
-
-//         animateLine();
-//     }
-// }
-
-// function animateLine() {
-//     if (!isDrawing) return;
-
-//     context.clearRect(0, 0, canvas.width, canvas.height);
-//     context.drawImage(preview, 0, 0);
-
-//     context.beginPath();
-//     context.moveTo(startX, startY);
-//     context.lineTo(endX, endY);
-//     context.strokeStyle = lineColor;
-//     context.lineWidth = lineWidth;
-//     context.stroke();
-
-//     // 修改線條的座標，這裡使用固定的速度
-//     startX += 1;
-//     endX += 1;
-
-//     requestAnimationFrame(animateLine);
-// }
-
-// 將包含線條的 Canvas 元素轉換為圖片並顯示
-// preview.onload = function () {
-//     preview.src = canvas.toDataURL();
-// };
